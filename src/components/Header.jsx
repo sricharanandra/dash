954def88f932186acfed12e43302cc5ab5b34f58
Sricharan Andra <sricharanandra7@gmail.com>
2024-06-06 19:44:46 +0530

    first commit + added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <>
+      <nav className="container">
+        <p className="ubuntu-medium">dash</p>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
